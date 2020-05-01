88329528c331185370d5b55cbdbc9dc75c206ea0
Juliet Goldstein <thealamogirlsings@gmail.com>
2020-05-01 18:24:31 -0500

    stuff

--- practice.js
+++ practice.js
@@ -1,7 +1,46 @@
-function password generator ( len) { 
-    var length = 
+function generate() {
+  let length = document.getElementById("length").value;
+  if (isNaN(length)) length = 16;
+  if (length < 8) length = 8;
+  if (length > 128) length = 128;
+
+  let pw_src = "";
+
+  if (document.getElementById("upper").checked) {
+    pw_src += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  }
+
+  if (document.getElementById("lower").checked) {
+    pw_src += "abcdefghijklmnopqrstuvwqyz";
+  }
+
+  if (document.getElementById("num").checked) {
+    pw_src += "0123456789";
+  }
+
+  if (document.getElementById("char").checked) {
+    pw_src += "!@#$%^&*";
+  }
+
+  let password = "";
+  const characters = pw_src;
+  //const characters = "0123456789, abcdefghijklmnopqrstuvwqyz, ABCDEFGHIJKLMNOPQRSTUVWXYZ, !@#$%^&*"
+
+  for (let i = 0; i < length; i++) {
+    // Start by setting i = 0
+    // Continue doing this loop as long as i<length
+    // Each time this  loop loops, increase i by 1 (i++)
+
+    let idx = Math.random() * characters.length;
+    idx = Math.floor(idx); // Change idx to an integer
+    password = password + characters[idx];
+  }
+
+  console.log(password);
+
+  let htmlOutput =
+    "<p>Your super secret password is:</p>\n<p>" + password + "</p>\n";
+
+  let e = document.getElementById("password");
+  e.innerHTML = htmlOutput;
 }
-
-Math.random()
-
-const characters = "0123456789, abcdefghijklmnopqrstuvwqyz, ABCDEFGHIJKLMNOPQRSTUVWXYZ, !@#$%^&*" 
